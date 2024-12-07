1abf3ff8c95c6970d9db2a74f28ecf6044a9b022
Jose-Barasa1 <barasajose90@gmail.com>
2024-12-07 13:14:33 +0300

    fixed bug for class article

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,50 +1,116 @@
 class Article:
     def __init__(self, author, magazine, title):
-        self.author = author
-        self.magazine = magazine
-        self.title = title
+        if not isinstance(author, Author):
+            raise ValueError("Author must be an instance of Author.")
+        if not isinstance(magazine, Magazine):
+            raise ValueError("Magazine must be an instance of Magazine.")
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            raise ValueError("Title must be a string between 5 and 50 characters.")
         
+        self._author = author
+        self._magazine = magazine
+        self._title = title
+        
+        author._articles.append(self)
+        magazine._articles.append(self)
+
+    @property
+    def title(self):
+        return self._title
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, new_author):
+        if not isinstance(new_author, Author):
+            raise ValueError("New author must be an instance of Author.")
+        # Remove article from old author's list if exists
+        if self._author:
+            self._author._articles.remove(self)
+        self._author = new_author
+        new_author._articles.append(self)
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, new_magazine):
+        if not isinstance(new_magazine, Magazine):
+            raise ValueError("New magazine must be an instance of Magazine.")
+        # Remove article from old magazine's list if exists
+        if self._magazine:
+            self._magazine._articles.remove(self)
+        self._magazine = new_magazine
+        new_magazine._articles.append(self)
+
+
 class Author:
     def __init__(self, name):
-        if not isinstance(name,str):
-            raise TypeError("name must be of typestring")
-        if len(name) == 0:
-            raise ValueError("name must be longer than 0 characters")
-        self.name = name
+        if not isinstance(name, str) or len(name) == 0:
+            raise ValueError("Name must be a non-empty string.")
+        self._name = name
+        self._articles = []
+
     @property
     def name(self):
         return self._name
 
     def articles(self):
-        return [article for article in Article.all() if article.author == self]
-        
-        
-    
-        
+        return self._articles
 
     def magazines(self):
-        return list({article.magazine for article in self.articles()})
-        pass
+        return list(set(article.magazine for article in self._articles))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+        return list(set(magazine.category for magazine in self.magazines()))
+
 
 class Magazine:
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
+        if not isinstance(name, str) or not (2 <= len(name) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters.")
+        if not isinstance(category, str) or len(category) == 0:
+            raise ValueError("Category must be a non-empty string.")
+        
+        self._name = name
+        self._category = category
+        self._articles = []
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        if not isinstance(new_name, str) or not (2 <= len(new_name) <= 16):
+            raise ValueError("Name must be a string between 2 and 16 characters.")
+        self._name = new_name
+
+    @property
+    def category(self):
+        return self._category
+
+    @category.setter
+    def category(self, new_category):
+        if not isinstance(new_category, str) or len(new_category) == 0:
+            raise ValueError("Category must be a non-empty string.")
+        self._category = new_category
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(set(article.author for article in self._articles))
 
     def article_titles(self):
-        pass
+        return [article.title for article in self._articles]
 
     def contributing_authors(self):
-        pass
+        return list(set(article.author for article in self._articles))
+mag1=Magazine(name="me",category="G")
